c60f2e467a317ceeb16c4a820374de25d684bb94
Kevin King <kevinturingking@gmail.com>
2022-04-20 10:14:05 -0600

    Add testing and data to the customer class

--- test/customer-test.js
+++ test/customer-test.js
@@ -2,4 +2,34 @@ import chai from 'chai';
 const expect = chai.expect;
 
 
-import Customer from '../classes/Customer'; 
+import Customer from '../classes/Customer';
+
+
+import { customerData } from '..sample-data/customerData';
+
+describe('Customer', () => {
+  let customer1
+  let customer2
+
+});
+
+  beforeEach(() => {
+    customer1 = new Customer(customerData[0]);
+    customer2 = new Customer(customerData[1]);
+  });
+
+  it('Should be a function', () => {
+    expect(Customer).to.be.a('function');
+  });
+
+  it('Should insantiate a new instance of a customer', () => {
+    expect(customer1).to.be.an.instanceOf(Customer);
+  });
+
+  it('Should have an id', () => {
+    expect(customer1.id).to.equal(1);
+  });
+
+  it('Should have a name', () => {
+    expect(customer1.name).to.equal('Leatha Ullrich');
+  });
