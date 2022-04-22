4e3a8ff7e0a88ce1a34f7621676ab443fa64cd34
Kevin King <kevinturingking@gmail.com>
2022-04-22 15:14:11 -0600

    Create fetch and post

---
+++ src/apiCalls.js
@@ -0,0 +1,30 @@
+let fetchData = (extension) => {
+  return fetch(`http://localhost:3001/api/v1/{extension}`)
+    .then((response) => {
+      if(resonse.ok) {
+        return response.json();
+      } else {
+        throw Error(response.statusText)
+      }
+    })
+    .catch(err => console.log(err));
+};
+
+const postData = (data) => {
+  return fetch(`http://localhost:3001/api/v1/bookings`, {
+    method: 'POST',
+    header: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(response => {
+      console.log(response.json())
+      throw "response"
+      }
+      return response.json()
+    })
+}
+
+
+export {fetchData, postData}
