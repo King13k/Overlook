dd7fbd4c40abb244f20414e20bc7fa26dc4b9782
Kevin King <kevinturingking@gmail.com>
2022-04-23 13:17:34 -0600

    Add user information to the page

--- src/scripts.js
+++ src/scripts.js
@@ -11,12 +11,59 @@ import './css/styles.css';
 console.log('This is the JavaScript entry file - your code begins here.');
 
 //imports
-import Customer from '../src/Classes/customer.js';
-import Booking from './src/Classes/booking.js';
-import Room from './src/Classes/rooms.js';
+import Customer from '../src/Classes/customer';
+import Booking from '../src/Classes/booking';
+import Room from '../src/Classes/rooms';
+// import domUpdates from './domUpdates';
+import {fetchData} from './apiCalls';
 
 
-// Query Selectors
+let customerData
+let bookingData
+let roomData
+let customer
+
+
+
+//Event Listeners
+window.addEventListener("load", function(){
+  Promise.all([fetchData('customers'), fetchData('bookings'), fetchData('rooms')])
+  .then(data => {
+      customerData = data[0].customers
+      bookingData = data[1].bookings
+      roomData = data[2].rooms
+      customer = new Customer(customerData[0])
+      viewUserDash(customer, bookingData, roomData)
+    })
+
+
+  // domUpdates.viewUserDash(customers, booking, roomData)
+});
 const userDashBoard = document.querySelector('.main-dash');
+const userBookingPage = document.querySelector('.dashboard');
 const roomBookBtn = document.querySelector('.navBtn');
-const userMessage = document.querySelector('welcome-message');
+const userMessage = document.querySelector('.welcome-user');
+
+
+
+
+function viewUserDash (customer, bookings, roomData) {
+  customer.getCustomerBookings(bookings)
+  const total = customer.getTotalSpent(roomData)
+  userMessage.innerHTML = `<h2>Welcome to Overlook Hotel ${customer.name},</h2>
+  <br><h2>Total Spent: $ ${total}</h2>`
+  if(customer.getCustomerBookings.length > 0) {
+    return customer.bookings.forEach(booking => {
+      userBookingPage.innerHTML += `
+      <section class="bookings">
+      <p>Booking Information:</p>
+      <p>confirmation number: ${booking.id}</p>
+      <p>Booked for: ${booking.date}<p>
+      <p>Room Number: ${booking.roomNumber}</p>
+      </section>`
+    })
+  } else {
+    userBookingPage.innerHTML = ''
+    return userBookingPage.innerHTML ='<h1>Sorry but at this time we do not have any available space, try again later</h1>'
+  }
+};
